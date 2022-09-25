347333604467ec8d477b4b09785723176265803c
ivan oparin <ivanopa.rin@yandex.ru>
2022-09-25 13:46:23 +0300

    db

--- firstlab/Models/DB.cs
+++ firstlab/Models/DB.cs
@@ -12,7 +12,7 @@
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql("Host=localhost;Port=54321;Database=personsdb;Username=postgres;Password=password");
+            optionsBuilder.UseNpgsql("Host=ec2-44-207-253-50.compute-1.amazonaws.com;Port=5432;Database=d93fsgtjvb8mhk;Username=sjhddzjabbauqx;Password=bb6f83ffedf446505e75867d9fdb3ea394b341d143ca284e7fcdbbba32a1ce13");
         }
     }
 }
