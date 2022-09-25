60369f9561555c99a562363a46e9f95e904781dd
ivan oparin <ivanopa.rin@yandex.ru>
2022-09-25 13:49:08 +0300

    app

---
+++ firstlab/Models/DB.cs
@@ -0,0 +1,18 @@
+﻿namespace firstlab.Models
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class DB : DbContext
+    {
+        public DbSet<Person> Persons { get; set; } = null!;
+
+        public DB()
+        {
+            
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=ec2-44-207-253-50.compute-1.amazonaws.com;Port=5432;Database=d93fsgtjvb8mhk;Username=sjhddzjabbauqx;Password=bb6f83ffedf446505e75867d9fdb3ea394b341d143ca284e7fcdbbba32a1ce13");
+        }
+    }
+}
---
+++ firstlab/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace firstlab.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Age { get; set; }
+        public string? Address { get; set; }
+        public string? Work { get; set; }
+    }
+}
